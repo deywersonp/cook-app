af613b8fe742c610cd6c6929d7f6ee5f3b60d497
Deywerson Pereira <deywerson.pereira@gmail.com>
2024-03-07 16:46:01 -0300

    feat: added ingredient component

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,8 +1,21 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { View, Text, ScrollView } from "react-native";
+
+import { Ingredient } from "@/components/Ingredient";
 
 import { styles } from "./styles";
 
 export default function Index() {
+  const [selected, setSelected] = React.useState<string[]>([]);
+
+  function handleToggleSelected(value: string) {
+    if (selected.includes(value)) {
+      return setSelected(state => state.filter(item => item !== value));
+    }
+
+    setSelected(state => [...state, value]);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
@@ -13,6 +26,21 @@ export default function Index() {
       <Text style={styles.message}>
         Descubra receitas baseadas nos  {"\n"}produtos que você escolheu.
       </Text>
+
+      <ScrollView
+        contentContainerStyle={styles.ingredients}
+        showsVerticalScrollIndicator={false}
+      >
+        {Array.from({ length: 100 }).map((_, index) => (
+          <Ingredient
+            key={index}
+            name=""
+            image=""
+            selected={selected.includes(String(index))}
+            onPress={() => handleToggleSelected(String(index))}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
